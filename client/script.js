498bf56a59001c852acf8602aaf123b951de9636
SureTech <suretech.manager@gmail.com>
2023-01-23 13:10:46 +1100

    update

--- client/script.js
+++ client/script.js
@@ -2,27 +2,28 @@ import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
 document.addEventListener("DOMContentLoaded", function(){
-  const form = document.querySelector('form')
-  const chatContainer = document.querySelector('#chat_container')
-  const iframe = document.getElementById("chat-iframe");
 
-  let loadInterval
 
-  function loader(element) {
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-      // Update the text content of the loading indicator
-      element.textContent += '.';
+        // Update the text content of the loading indicator
+        element.textContent += '.';
 
-      // If the loading indicator has reached three dots, reset it
-      if (element.textContent === '....') {
-        element.textContent = '';
-      }
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
     }, 300);
-  }
+}
 
-  function typeText(element, text) {
+function typeText(element, text) {
     let index = 0
 
     let interval = setInterval(() => {
@@ -33,44 +34,47 @@ document.addEventListener("DOMContentLoaded", function(){
             clearInterval(interval)
         }
     }, 20)
-  }
+}
 
-  // generate unique ID for each message div of bot
-  // necessary for typing text effect for that specific reply
-  // without unique ID, typing text will work on every element
-  function generateUniqueId() {
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
     const hexadecimalString = randomNumber.toString(16);
 
     return `id-${timestamp}-${hexadecimalString}`;
-  }
+}
 
-  function chatStripe(isAi, value, uniqueId) {
+function chatStripe(isAi, value, uniqueId) {
     return (
-      `
-      <div class="wrapper ${isAi && 'ai'}">
-        <div class="chat">
-          <div class="profile">
-            <img 
-              src=${isAi ? bot : user} 
-              alt="${isAi ? 'bot' : 'user'}" 
-            />
-          </div>
-          <div class="message" id=${uniqueId}>${value}</div>
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-      </div>
     `
     )
-  }
+}
 
-  function scrollToBottom() {
+function scrollToBottom() {
+    const iframe = document.getElementById("chat-iframe");
     iframe.contentDocument.body.scrollTop = iframe.contentDocument.body.scrollHeight;
-  }
+}
 
-  const handleSubmit = async (e) => {
+
+
+const handleSubmit = async (e) => {
     e.preventDefault()
-
+    
     const data = new FormData(form)
 
     // user's chatstripe
